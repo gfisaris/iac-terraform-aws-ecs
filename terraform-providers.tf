3b1b188e3e83575daac7c8f032f41fac4cd6c8dc
gfisaris <gfisaris@gmail.com>
2016-11-17 13:40:02 +0000

    FIX missing Variables

--- terraform-providers.tf
+++ terraform-providers.tf
@@ -1,8 +1,5 @@
-############################
-##  Predefined Variables  ##
-############################
-
-##  AWS Account Information  ##
-  tf_provider_aws_region      = "eu-central-1"
-  tf_provider_aws_access_key  = "AKIAI6ZHJMDTEPY4UO5Q"
-  tf_provider_aws_secret_key  = "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"
+provider "aws" {
+  access_key = "${var.tf_provider_aws_access_key}"
+  secret_key = "${var.tf_provider_aws_secret_key}"
+  region     = "${var.tf_provider_aws_region}"
+}
