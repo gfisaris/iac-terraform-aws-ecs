e3d792e5a25729bd45a7d16858c8a6d4b835bd53
gfisaris <gfisaris@gmail.com>
2016-11-18 13:23:00 +0000

    Resource Renaming

--- terraform-imported-sources.tf
+++ terraform-imported-sources.tf
@@ -25,3 +25,11 @@ data "terraform_remote_state" "aws_ec2_alb" {
     path = "${path.module}/../../aws-ec2-alb/master/terraform.tfstate"
   }
 }
+
+
+data "terraform_remote_state" "aws_ecs" {
+  backend = "local"
+  config {
+    path = "${path.module}/../../aws-ecs/master/terraform.tfstate"
+  }
+}
