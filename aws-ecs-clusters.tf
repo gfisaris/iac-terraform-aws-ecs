69a600cd1f195fc469a24f9f870a1c1def4a8fd6
gfisaris <gfisaris@gmail.com>
2016-11-19 16:25:42 +0000

    Massive Resource Renaming

--- aws-ecs-clusters.tf
+++ aws-ecs-clusters.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "master" {
-  name = "${data.terraform_remote_state.aws_dcs.region_name}"
+  name = "${data.terraform_remote_state.aws_ds.region_name}"
 }
 
 output "cluster_master_id"    { value = "${aws_ecs_cluster.master.id}" }
--- aws-ecs-services.tf
+++ aws-ecs-services.tf
@@ -8,10 +8,10 @@ resource "aws_ecs_service" "lep_stack" {
   deployment_maximum_percent = "200"
   deployment_minimum_healthy_percent = "100"
 
-  iam_role = "${data.terraform_remote_state.aws_iam.ecs_cluster_iamRole_ecsServiceRole_arn}"
+  iam_role = "${data.terraform_remote_state.aws_iam.role_ecsServiceRole_arn}"
 
   load_balancer {
-    target_group_arn = "${data.terraform_remote_state.aws_ec2_alb.alb_ecs_cluster_external_target_group_http_80_arn}"
+    target_group_arn = "${data.terraform_remote_state.aws_ec2_alb.target_group_external_http_80_arn}"
     container_name = "nginx"
     container_port = "80"
   }
@@ -20,5 +20,5 @@ resource "aws_ecs_service" "lep_stack" {
 output "service_lep_stack_id"             { value = "${aws_ecs_service.lep_stack.id}" }
 output "service_lep_stack_name"           { value = "${aws_ecs_service.lep_stack.name}" }
 output "service_lep_stack_cluster"        { value = "${aws_ecs_service.lep_stack.cluster}" }
-output "service_lep_stack_iamRole"        { value = "${aws_ecs_service.lep_stack.iam_role}" }
+output "service_lep_stack_iam_role"       { value = "${aws_ecs_service.lep_stack.iam_role}" }
 output "service_lep_stack_desired_count"  { value = "${aws_ecs_service.lep_stack.desired_count}" }
